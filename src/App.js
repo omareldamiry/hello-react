07a33374c8ee655bd41ea4cf8eda071c849922d3
unknown <omar.eldamiry@gmail.com>
2022-06-02 12:54:42 +0200

    ReactFacts

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import Main from './components/Main';
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Main />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="title">
+                <img src="../logo192.png" className="icon" alt="react-logo" />
+                <h3>ReactFacts</h3>
+            </div>
+            <h4 className="nav-item">
+                React Course - Project 1
+            </h4>
+        </nav>
+    );
+};
+
+export default Navbar;
